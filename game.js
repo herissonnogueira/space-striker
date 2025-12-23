e8bd09f08efc0bcd47fc1d5be8763ecf21fa76d4
Herisson Nogueira <herisson.nogueira002@gmail.com>
2025-12-22 21:28:29 -0300

    Implement player ship and keyboard controls

--- game.js
+++ game.js
@@ -41,6 +41,72 @@ for (let i = 0; i < 100; i++) {
     stars.push(new Star());
 }
 
+// Classe do jogador
+class Player {
+    constructor() {
+        this.width = 40;
+        this.height = 40;
+        this.x = canvas.width / 2 - this.width / 2;
+        this.y = canvas.height - this.height - 20;
+        this.speed = 5;
+        this.movingLeft = false;
+        this.movingRight = false;
+    }
+
+    update() {
+        if (this.movingLeft && this.x > 0) {
+            this.x -= this.speed;
+        }
+        if (this.movingRight && this.x < canvas.width - this.width) {
+            this.x += this.speed;
+        }
+    }
+
+    draw() {
+        // Desenhar nave (triângulo)
+        ctx.fillStyle = '#00ffff';
+        ctx.beginPath();
+        ctx.moveTo(this.x + this.width / 2, this.y);
+        ctx.lineTo(this.x, this.y + this.height);
+        ctx.lineTo(this.x + this.width, this.y + this.height);
+        ctx.closePath();
+        ctx.fill();
+
+        // Brilho da nave
+        ctx.strokeStyle = '#00ffff';
+        ctx.lineWidth = 2;
+        ctx.stroke();
+    }
+}
+
+// Criar jogador
+const player = new Player();
+
+// Controles do teclado
+const keys = {};
+
+document.addEventListener('keydown', (e) => {
+    keys[e.key] = true;
+
+    if (e.key === 'ArrowLeft') {
+        player.movingLeft = true;
+    }
+    if (e.key === 'ArrowRight') {
+        player.movingRight = true;
+    }
+});
+
+document.addEventListener('keyup', (e) => {
+    keys[e.key] = false;
+
+    if (e.key === 'ArrowLeft') {
+        player.movingLeft = false;
+    }
+    if (e.key === 'ArrowRight') {
+        player.movingRight = false;
+    }
+});
+
 // Game loop
 function gameLoop() {
     // Limpar canvas
@@ -52,6 +118,10 @@ function gameLoop() {
         star.update();
         star.draw();
     });
+
+    // Atualizar e desenhar jogador
+    player.update();
+    player.draw();
 
     requestAnimationFrame(gameLoop);
 }
