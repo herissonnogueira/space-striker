cf1f4c47cf102c8d1fb3f029914226da3ea85881
Herisson Nogueira <herisson.nogueira002@gmail.com>
2025-12-22 21:22:58 -0300

    Add animated star field with parallax effect

--- game.js
+++ game.js
@@ -6,19 +6,52 @@ const ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 600;
 
-// Game loop básico
+// Sistema de estrelas com parallax
+class Star {
+    constructor() {
+        this.reset();
+    }
+
+    reset() {
+        this.x = Math.random() * canvas.width;
+        this.y = Math.random() * canvas.height;
+        this.size = Math.random() * 2;
+        this.speed = this.size * 0.5;
+        this.opacity = Math.random() * 0.5 + 0.5;
+    }
+
+    update() {
+        this.y += this.speed;
+
+        if (this.y > canvas.height) {
+            this.y = 0;
+            this.x = Math.random() * canvas.width;
+        }
+    }
+
+    draw() {
+        ctx.fillStyle = `rgba(255, 255, 255, ${this.opacity})`;
+        ctx.fillRect(this.x, this.y, this.size, this.size);
+    }
+}
+
+// Criar array de estrelas
+const stars = [];
+for (let i = 0; i < 100; i++) {
+    stars.push(new Star());
+}
+
+// Game loop
 function gameLoop() {
     // Limpar canvas
     ctx.fillStyle = '#000';
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    // Desenhar estrelas de fundo (placeholder)
-    ctx.fillStyle = '#fff';
-    for (let i = 0; i < 50; i++) {
-        const x = Math.random() * canvas.width;
-        const y = Math.random() * canvas.height;
-        ctx.fillRect(x, y, 2, 2);
-    }
+    // Atualizar e desenhar estrelas
+    stars.forEach(star => {
+        star.update();
+        star.draw();
+    });
 
     requestAnimationFrame(gameLoop);
 }
